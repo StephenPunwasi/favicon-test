9dd1c48bec59ff5c181b97680cd6f27cf999ac76
Stephen Punwasi <stephen.punwasi@gmail.com>
2014-12-22 13:26:30 -0500

    Adds A Favicon To Your Wordpress Theme
    Properly adds a favicon to your theme

---
+++ favicon.php
@@ -0,0 +1,20 @@
+<?php
+/*
+Plugin Name: Favicon Loader
+Plugin URI:
+Description: Adds a favicon to your page.
+interface
+Version: 1.0
+Author: Stephen Punwasi
+License: GPLv2
+*/
+
+add_action('wp_head', 'icon_page_output');
+function icon_page_output(){
+  $icon_url = plugins_url('favicon.ico', __FILE__);
+  ?>
+  <link rel="shortcut icon" href="<?php echo $icon_url; ?>"/>
+  <?php
+}
+
+?>
